6973dcd12d627d24a2a30a17a89de676f4c85d8d
Abbieho <liv7@msn.com>
2015-07-12 15:46:05 +0800

    no message

--- plot2.R
+++ plot2.R
@@ -10,7 +10,8 @@ data$Date <- strptime(data$Date, "%d/%m/%Y")
 data$Datetime <- strptime(paste(data$Date,data$Time, sep=" "), "%Y-%m-%d %H:%M:%S")
 sub_data <- subset(data, as.Date(Date) == "2007-02-01" | as.Date(Date) == "2007-02-02")
 
-#plot 1
-png("plot1.png", width = 480, height = 480)
-hist(sub_data$Global_active_power, main="Global Active Power",xlab="Global Active Power (kilowatts)",col="red")
+# plot2
+Sys.setlocale("LC_TIME", "en_US.UTF-8")
+png("plot2.png", width = 480, height = 480)
+plot(sub_data$Datetime,sub_data$Global_active_power, type="l",xlab="" , ylab = "Global Active Power (kilowatts)")
 dev.off()
